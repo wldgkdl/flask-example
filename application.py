1970762a156291f87c6c8c2275e38bb2159f8e60
wldgkdl <wldgkdl2@gmail.com>
2022-10-01 20:49:15 -0400

    small change test

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route('/')    # default web page
 def hello_world():
-	return 'Welcome to Kevin & Kim factory'
+	return 'Welcome to Kevin & Kim factory. This code comes from git now'
