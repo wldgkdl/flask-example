39dae8edd44f048d050525ba3691a4faa3946d08
wldgkdl <wldgkdl2@gmail.com>
2022-10-30 20:00:51 -0400

    link with index file

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from flask import Flask 
+from flask import Flask, render_template
+import os
 
 application = Flask(__name__)
 
 @application.route('/')    # default web page
-def hello_world():
-	return 'Welcome to Kevin & Kim factory. This code comes from git now'
+def welcome_page():
+	return render_template('index.html')
+	# return 'Welcome to Kevin & Kim factory. This code comes from git now'
+
+
